30eef9c46e18d34658428b249ec660944f2296ce
+ <+>
2024-01-13 22:10:42 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(arrayOfObjects){
+    const result = arrayOfObjects.find(obj => obj.result === "W");
+    if (result) {
+       return result.year;
+    } else {
+        return undefined;
+    }
+
+}
